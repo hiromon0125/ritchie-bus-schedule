185e620d615012d459fed53ec3dfd72eca27f5dc
hiromon0125 <hiroto125takeuchi@gmail.com>
2024-01-20 04:12:55 -0500

    feat: update metadata

---
+++ src/app/manifest.ts
@@ -0,0 +1,11 @@
+export const manifest = {
+  name: "Ritchie Bus Schedule",
+  short_name: "Ritchie Bus",
+  description: "Best way to find the RIT bus schedule",
+  start_url: "/",
+  display: "standalone",
+  background_color: "#fff",
+  theme_color: "#fff",
+  categories: ["travel", "education", "productivity", "utilities"],
+  orientation: "portrait",
+};
