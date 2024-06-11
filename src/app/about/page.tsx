c495cab11ca393fcccf234d98d7be0f4361e516e
hiromon0125 <hiroto125takeuchi@gmail.com>
2024-06-11 01:20:18 -0400

    feat: about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/header";
+
+async function Page() {
+  return (
+    <>
+      <Header />
+      <div className=" w-full max-w-screen-lg px-6">Hello!</div>
+    </>
+  );
+}
+
+export default Page;
