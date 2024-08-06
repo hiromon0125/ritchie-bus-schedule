1a6252f182f394c287b455ad2e90fc140fc80f0b
hiromon0125 <hiroto125takeuchi@gmail.com>
2024-08-06 00:16:38 -0400

    fix: og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/ritches-bus-schedule-banner.png",
-        secureUrl: "/ritches-bus-schedule-banner.png",
         width: 1200,
         height: 630,
         alt: "RIT Bus Schedule",
