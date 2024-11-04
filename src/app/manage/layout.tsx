0d810365f463428722763f087ce1a20e40148188
Hiroto Takeuchi <hiroto125takeuchi@gmail.com>
2024-11-04 14:33:01 -0500

    Feat/animator (#20)
    * fix: ui for home screen
    
    * feat: bus and stop list
    
    * feat: request for favoriting
    
    * fix: import issues
    
    * feat: up nextjs, fav bus/stop, header color
    
    * fix: icon color
    
    * fix: i have no clue why eslint is angy
    
    * feat: add fav stop to home
    
    * feat: map, complete refreshed stop page, fixed bus stop page
    
    * fix: font size
    
    * feat: add pp
    
    * fix: compiling build
    
    * feat: update clerk, eslint, pnpm. add sharp for nextjs image
    
    * fix: eslint
    
    * fix: downgrade clerk as typing is not strictly applied
    
    * fix: logged out account screen, clerk error
    
    * fix: unauthorized errors and add terms button on signni
    
    * feat: settings and account management pages
    
    * feat: update about page
    
    * fix: order of routes in stop page, missing stop location
    
    * fix: spacing in the stop page header
    
    * fix: gap size
    
    * feat: updated readme file
    
    * Update README.md
    
    add banner
    
    * Update README.md
    
    Add more links
    
    * fix: sitemap, metadata, and readme file
    
    * feat: rating widget
    
    * feat: Bus ui refresh part 1
    
    * fix: status and bus page
    
    * fix: hydration error and stop name not showing
    
    * feat: animate bus widgets
    
    * feat: loading screen for bus page, fix: hydration error on header
    
    * feat: update to nextjs, clerkjs
    
    * fix: build
    
    * fix: build issue
    
    * feat: solve loading issue
    
    * feat: compilation, converting input field to UTC date
    
    * fix: build
    
    * feat: status marker on timetable
    
    * fix: mov to parallel paths
    
    * fix: timezone issues
    
    * fix: manage screen redirecting for example user

--- src/app/manage/layout.tsx
+++ src/app/manage/layout.tsx
@@ -1,23 +1,11 @@
 import Header from "@/header";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 import React from "react";
-import { api } from "t/server";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { userId } = await auth();
-  if (userId == null) {
-    redirect("/");
-  }
-  const manager = await api.manager.isManager.query({ userId: userId });
-  if (!manager) {
-    redirect("/");
-  }
-
   return (
     <main className=" flex min-h-screen w-full flex-col items-center bg-slate-100 text-black">
       <Header />
