42f54f9ed9bd16002e958c9f557fa4eac591065c
Hiro <hiroto125takeuchi@gmail.com>
2025-03-06 22:42:51 -0500

    fix: build error

--- src/app/api/serviceinfo/[secretkey]/route.ts
+++ src/app/api/serviceinfo/[secretkey]/route.ts
@@ -1,7 +1,11 @@
+import { type NextRequest } from "next/server";
 import { env } from "process";
 import { updateServiceInfo } from "../route";
 
-async function handler({ params }: { params: Promise<{ secretkey: string }> }) {
+async function handler(
+  _: NextRequest,
+  { params }: { params: Promise<{ secretkey: string }> },
+) {
   const { secretkey } = await params;
   if (secretkey !== env.NEXT_SERVICE_INFO_SECRET_KEY) {
     return new Response("Invalid secret key", { status: 401 });
