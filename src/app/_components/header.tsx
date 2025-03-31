6e3fe67765b873c19d477ea75bf1a6e086dd5823
Hiro <hiroto125takeuchi@gmail.com>
2025-03-31 00:50:37 -0400

    fix: rename dynamic component tag name

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -17,7 +17,7 @@ export default function Header({
   serviceNavigation?: React.ReactNode;
 }) {
   const path = usePathname();
-  const Header = PATH_TO_HEADER_TAG[path] ?? "p";
+  const TitleTag = PATH_TO_HEADER_TAG[path] ?? "p";
   return (
     <div className="xs:px-3 text-foreground top-2 z-20 w-full px-1 md:sticky">
       <div className="bg-border-background md:bg-border-background/60 m-2 mx-auto h-24 w-full justify-center rounded-3xl border-slate-700 p-3 md:max-w-(--breakpoint-lg) md:backdrop-blur-md not-dark:md:shadow-md dark:border">
@@ -34,9 +34,9 @@ export default function Header({
               title="Ritchie's Bus Schedule Logo"
               loading="eager"
             />
-            <Header className="m-0 text-lg font-semibold max-[450px]:hidden min-[850px]:text-xl lg:text-2xl">
+            <TitleTag className="m-0 text-lg font-semibold max-[450px]:hidden min-[850px]:text-xl lg:text-2xl">
               Ritchie's Bus Schedule
-            </Header>
+            </TitleTag>
           </Link>
           <nav
             className="hidden h-full flex-row items-center gap-[5px] rounded-xl bg-neutral-500/60 p-[5px] text-xl not-dark:shadow-[0px_2px_2px_-1px_var(--black-shadow-color)_inset,0px_-2px_4px_-1px_var(--white-shadow-color)_inset,0px_1px_1px_0px_var(--white-highlight-color)] md:flex dark:bg-slate-950/60"
