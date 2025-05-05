53b65575422b456cd6c10852638692d28ae200be
jtb8595 <145400040+jtb8595@users.noreply.github.com>
2025-05-05 01:48:49 +0000

    Unique Meta Descriptions for Subroutes (#132)
    * Added meta description for stops
    
    Added a generateMetadata function following the description for issue 104
    
    * Added meta description for the bus list page
    
    Added a metadata constant to app/buses/page.tsx following the described solution in issue 105
    
    * Added meta description for the stop list page
    
    -Added a metadata constant to app/stops/page.tsx as described in the solution requested in issue 106
    
    -Added trailing comma to description field of metadata constant in buses/page/tsx added in previous commit
    
    * Added meta description to the settings page
    
    -Added a metadata constant to app/setting/page.tsx as described in the requested solution in Issue 107
    
    * Updated title for bus page
    
    Updated the title for the bus page to align with the added metadata tags as requested in Issue 108
    
    ---------
    
    Co-authored-by: Hiroto Takeuchi <hiroto125takeuchi@gmail.com>

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,4 +1,10 @@
 import Settings from "@/settings";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Account Settings | RIT Bus Schedule",
+  description: "Customize your RIT Bus Schedule profile, update your profile picture or change preferences",
+}
 
 export default function SettingPage() {
   return (
