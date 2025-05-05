3ef30fd0310669aeb983d5256202a3eec4865201
Hiro <hiroto125takeuchi@gmail.com>
2025-05-05 00:17:25 -0400

    feat: add title

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,6 +6,10 @@ import { version } from "../../package.json";
 import { Button } from "../components/ui/button";
 import { BackBtn } from "./_components/backBtn";
 
+export const metadata = {
+  title: "404 Not found | Ritchie's Bus Schedule",
+};
+
 export default function NotFound() {
   return (
     <main className="text-foreground xs:[--margin:24px] flex min-h-screen w-full flex-col items-center gap-3 py-2 [--margin:8px] [--sm-max-w:calc(100%-var(--margin))]">
