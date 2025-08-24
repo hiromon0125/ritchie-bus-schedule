99721fea011bbd2b701ba7d19eac4749ff569875
Hiro <hiroto125takeuchi@gmail.com>
2025-08-24 17:45:35 -0400

    feat: reset cache in manage page

---
+++ src/app/_components/editors/flushCache.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useActionState, useEffect, useRef } from "react";
+import { flushAllCache } from "./manageAction";
+
+export function FlushCacheBtn() {
+  const mounted = useRef(false);
+  const [count, formAction, isPending] = useActionState(flushAllCache, 0);
+  useEffect(() => {
+    if (!mounted.current || isPending) {
+      mounted.current = true;
+      return;
+    }
+    alert(`Flushed ${count} cache entries`);
+  }, [isPending, count]);
+  return (
+    <form action={formAction}>
+      <button
+        type="submit"
+        className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700 disabled:opacity-50"
+        disabled={isPending}
+      >
+        Flush All Cache
+      </button>
+    </form>
+  );
+}
+
+export default function FlushCache() {
+  return (
+    <div>
+      <p>Flush All Cache</p>
+      <FlushCacheBtn />
+    </div>
+  );
+}
--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,13 +1,24 @@
 import EditBusList from "@/editors/editBusList";
 import EditStopList from "@/editors/editStopList";
+import { FlushCacheBtn } from "../_components/editors/flushCache";
 
 async function Page() {
   return (
-    <div className=" w-full max-w-(--breakpoint-lg) px-6 pb-4">
-      <h1>Bus List</h1>
+    <div className="w-full max-w-(--breakpoint-lg) px-6 pb-4">
+      <h1>Manage Buses</h1>
+      <h2 className="text-2xl">Bus List</h2>
       <EditBusList />
-      <h1 className=" mt-10">Station List</h1>
+      <h2 className="mt-10 text-2xl">Station List</h2>
       <EditStopList />
+      <div className="mt-10 flex flex-col gap-2 rounded-md border-2 border-red-400 bg-red-100 p-3">
+        <h2 className="text-2xl">Reset Cache</h2>
+        <p className="font-light">
+          Use this in case any old cache gets left behind after editing. Do not
+          use this often as it will increase server cost and reduce load time
+          for users.
+        </p>
+        <FlushCacheBtn />
+      </div>
     </div>
   );
 }
